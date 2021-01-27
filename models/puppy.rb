67bd696dc22ed0cde8bdb0426573d6b0e7669aac
Romano Livi <romano_livi@outlook.com>
2021-01-27 19:04:34 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,9 @@
 class Puppy
   attr_accessor :name, :breed, :age 
   
-  def initialize(name, breed, age)
-    @name = name 
-    @breed = breed
-    @age = age
+  def initialize(params)
+    @name = params[:name] 
+    @breed = params[:breed]
+    @age = params[:age]
   end
 end
